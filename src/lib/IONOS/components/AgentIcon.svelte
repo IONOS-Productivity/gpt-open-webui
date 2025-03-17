3d84a76f2cebd9cc17eb3fa5d0c1a64b1adbdf78
Thomas Lehmann <t.lehmann@strato.de>
2025-03-17 19:30:44 +0100

    [POC] c/feat(SmallAgentSelector): support agent icons via special tag icon:<name>
    TODO:  Define "code" icon
    
    If an agent defines an icon via a tag "icon:<name>" an
    icon <name> is added.
    
    Allowed icons are: talk, code, image, pencil
    
    Refs: PRODAI-126

---
+++ src/lib/IONOS/components/AgentIcon.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import Pencil from '$lib/components/icons/Pencil.svelte';
+	import PhotoSolid from '$lib/components/icons/PhotoSolid.svelte';
+	import Heart from '$lib/components/icons/Heart.svelte';
+	import ChatBubbleOval from '$lib/components/icons/ChatBubbleOval.svelte';
+
+	export let iconName = '';
+</script>
+
+{#if iconName == 'talk'}
+	<ChatBubbleOval className="inline-block size-4" />
+{:else if iconName == 'code'}
+	<Heart className="inline-block size-4" />
+{:else if iconName == 'image'}
+	<PhotoSolid className="inline-block size-4" />
+{:else if iconName == 'pencil'}
+	<Pencil className="inline-block size-4" />
+{:else}
+	<Pencil className="inline-block size-4" />
+{/if}
