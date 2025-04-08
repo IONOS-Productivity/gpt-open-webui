1fef598b5005f153ac57b76d1673f9c2a13d9394
Thomas Lehmann <t.lehmann@strato.de>
2025-04-08 12:17:24 +0200

    c/feat(IONOS): add link component
    Colors not final.
    
    Refs: PRODAI-144

---
+++ src/lib/IONOS/components/common/Link.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let href: string = '';
+</script>
+
+<a
+	{...$$restProps}
+	{href}
+	class="text-blue-700 hover:text-blue-500 active:text-blue-300 hover:underline"
+	target="_blank"
+	rel="noreferrer"
+>
+	<slot />
+</a>
