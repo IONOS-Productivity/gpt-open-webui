ce7f806c275387a0f0c97e960540870abe66be3e
Thomas Lehmann <t.lehmann@strato.de>
2025-04-11 17:01:24 +0200

    c/feat(IONOS/Filepicker): add capability to make it look like Button
    For use in the settings dialogs as chat import button.
    
    Refs: PRODAI-90

--- src/lib/IONOS/components/common/Filepicker.svelte
+++ src/lib/IONOS/components/common/Filepicker.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
 	import { createEventDispatcher } from 'svelte';
+	import Button, { ButtonType } from '$lib/IONOS/components/common/Button.svelte'
 	const dispatch = createEventDispatcher();
 
+	export let type: ButtonType|null = null;
+	export let accept: string = '*';
+	export let multiple: boolean = true;
+
+	let inputEl: HTMLInputElement|null = null;
 	let files: FileList;
 	let value = '';
 
@@ -16,13 +22,26 @@
 </script>
 
 <label class="cursor-pointer">
-	<slot />
+	{#if type === null}
+		<slot />
+	{/if}
 	<input
 		type="file"
 		bind:value={value}
 		bind:files={files}
-		multiple
+		bind:this={inputEl}
+		{accept}
+		{multiple}
 		on:change={change}
 		class="hidden"
 	/>
 </label>
+
+{#if type !== null}
+	<Button
+		on:click={() => { inputEl?.click?.(); } }
+		{type}
+	>
+		<slot />
+	</Button>
+{/if}
