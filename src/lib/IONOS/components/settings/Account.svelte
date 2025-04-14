12385d703219ddb8e996313dd34dd64e6a84a01a
Thomas Lehmann <t.lehmann@strato.de>
2025-04-14 14:34:32 +0200

    [WIP] Add UI for delete account *** TODOs
    TODO:
    
    * Service not implemented yet

--- src/lib/IONOS/components/settings/Account.svelte
+++ src/lib/IONOS/components/settings/Account.svelte
@@ -7,6 +7,12 @@
 	import { user } from '$lib/stores';
 	import { resetPassword } from '$lib/IONOS/services/account';
 	const i18n = getContext<Readable<I18Next>>('i18n');
+
+	let confirmAccountDeletion = false;
+
+	function onDeleteAccountConfirmed() {
+		console.warn('Implement me: account deletion');
+	}
 </script>
 
 <div class="flex flex-col gap-5">
@@ -30,4 +36,25 @@
 			</Button>
 		</div>
 	</div>
+	<div class="flex flex-row items-center h-10 hidden">
+		<div class="flex-grow">
+			{$i18n.t('Delete account', { ns: 'ionos' })}
+		</div>
+		<div>
+			<Button
+				on:click={() => { confirmAccountDeletion = true; }}
+				type={ButtonType.caution}>
+				{$i18n.t('Delete account', { ns: 'ionos' })}
+			</Button>
+		</div>
+	</div>
 </div>
+
+<Confirm
+	title={$i18n.t('Do you want to delete your account?', { ns: 'ionos' })}
+	message={$i18n.t('This action can not be undone', { ns: 'ionos' })}
+	show={confirmAccountDeletion}
+	confirmText={$i18n.t('Delete my account', { ns: 'ionos' })}
+	confirmHandler={onDeleteAccountConfirmed}
+	cancelHandler={() => { confirmAccountDeletion = false; }}
+/>
