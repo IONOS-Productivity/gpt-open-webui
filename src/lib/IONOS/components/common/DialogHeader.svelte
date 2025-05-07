6d38bf44a1f4a87bba3bf2148ea623014dcc0883
Kai Henseler <kai.henseler@strato.de>
2025-05-07 14:12:12 +0200

    c/refactor(Dialogs): make dialogs more flexible
    Signed-off-by: Kai Henseler <kai.henseler@strato.de>

---
+++ src/lib/IONOS/components/common/DialogHeader.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import type { Readable } from 'svelte/store';
+	import type { I18Next } from '$lib/IONOS/i18next.d.ts';
+	import { getContext } from 'svelte';
+	import { createEventDispatcher } from 'svelte';
+	import XMark from '$lib/IONOS/components/icons/XMark.svelte';
+
+	const dispatch = createEventDispatcher();
+
+	const i18n = getContext<Readable<I18Next>>('i18n');
+
+	export let closable = true;
+	export let title = '';
+	export let dialogId = title;
+</script>
+
+<div class="flex items-center justify-between {$$props.class}" class:border-b={closable}>
+	<h1 class="font-semibold text-sm text-blue-800">
+		{title}
+	</h1>
+	{#if closable}
+		<button class="justify-self-end" data-id="dialog-close-{dialogId}" on:click={() => dispatch('close') }>
+			<XMark />
+		</button>
+	{/if}
+</div>
