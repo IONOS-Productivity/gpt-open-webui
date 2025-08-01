9a7e32d5553cc2714ee4949f82938a0fe37f3949
Thomas Lehmann <t.lehmann@strato.de>
2025-08-01 10:53:31 +0200

    c/feat(Footer): show LLM hint + info menu in column (mobile)
    According to mobile Figma.
    
    Refs: PRODAI-332

--- src/lib/IONOS/components/Footer.svelte
+++ src/lib/IONOS/components/Footer.svelte
@@ -8,16 +8,18 @@
 	let showTerms: boolean = false;
 </script>
 
-<div class="flex md:flex-row flex-col justify-center mx-auto mb-7 max-w-2xl text-gray-400 text-xs gap-[3px] text-center">
-	<p>
-		{$i18n.t('IONOS GPT can make mistakes.', { ns: 'ionos' })}
-	</p>
-	<TermsMenu
-		show={showTerms}
-		className="text-blue-700"
-	>
-		<div class="font-semibold underline">
-			{$i18n.t('Check important info.', { ns: 'ionos' })}
-		</div>
-	</TermsMenu>
+<div class="flex flex-col items-center">
+	<div class="flex max-md:flex-col mb-7 max-w-2xl text-gray-400 text-xs text-center gap-[3px] max-md:gap-[10px]">
+		<p>
+			{$i18n.t('IONOS GPT can make mistakes.', { ns: 'ionos' })}
+		</p>
+		<TermsMenu
+			show={showTerms}
+			className="text-blue-700"
+		>
+			<div class="font-semibold underline">
+				{$i18n.t('Check important info.', { ns: 'ionos' })}
+			</div>
+		</TermsMenu>
+	</div>
 </div>
