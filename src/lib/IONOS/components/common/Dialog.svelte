103293f9c129ba711eb8955331174a9c9e75adf8
Thomas Lehmann <t.lehmann@strato.de>
2025-08-05 15:41:36 +0200

    [DEBUG] align-items: flex-start for the dialog to disable centering
    Observation: Now the offset is correct, transition still broken.

--- src/lib/IONOS/components/common/Dialog.svelte
+++ src/lib/IONOS/components/common/Dialog.svelte
@@ -22,7 +22,7 @@
 
 <dialog
 	bind:this={el}
-	class="fixed top-0 right-0 left-0 bottom-0 m-0 bg-black/75 h-screen max-w-[100vw] w-[100vw] max-h-[100dvh] justify-center items-center z-[99999999] overflow-hidden overscroll-contain {positioning}">
+	class="fixed top-0 right-0 left-0 bottom-0 m-0 bg-black/75 h-screen max-w-[100vw] w-[100vw] max-h-[100dvh] justify-center items-center z-[99999999] overflow-hidden overscroll-contain {positioning} items-start">
 	<div
 		data-id={`dialog-${dialogId}`}
 		class="{$$props.class ?? 'p-[30px]'} flex flex-col bg-white relative shadow-xl rounded-2xl md:translate-none"
