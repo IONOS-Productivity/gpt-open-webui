e7b9b88b1d0e0f4d69423350f64ec45764395910
Kai Henseler <kai.henseler@strato.de>
2025-08-05 13:00:51 +0200

    c/fix(PrivacySlogan): not visible for anonymous users
    Refs: PRODAI-335
    
    Signed-off-by: Kai Henseler <kai.henseler@strato.de>

--- src/lib/IONOS/components/PrivacySlogan.svelte
+++ src/lib/IONOS/components/PrivacySlogan.svelte
@@ -7,7 +7,7 @@
 	const i18n = getContext<Readable<I18Next>>('i18n');
 </script>
 
-<div class="flex flex-row mx-10 items-center">
+<div class="flex flex-row md:mx-10 items-center">
 	<a
 		href="https://www.ionos.de/ionos-gpt-datenschutz-manifest#digitale-loesungen"
 		target="_blank"
@@ -15,7 +15,7 @@
 	>
 		<Shield className={"h-6 w-6 inline-block mr-2 fill-purple-700"} />
 
-		<span class="text-purple-700">
+		<span class="text-purple-700 md:inline-block hidden">
 			{$i18n.t('Privacy by Design!', { ns: 'ionos' })}
 		</span>
 	</a>
