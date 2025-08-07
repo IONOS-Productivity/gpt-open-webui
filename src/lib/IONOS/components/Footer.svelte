fba4de326150c4140ef832fec0bb1de38448ce38
Kai Henseler <kai.henseler@strato.de>
2025-08-07 15:47:19 +0200

    c/style(Footer): align texts below each other on mobile
    As the text wraps badly on small screens and flex-col looks better. Not according to Figma
    
    Refs: PRODAI-334
    
    Signed-off-by: Kai Henseler <kai.henseler@strato.de>

--- src/lib/IONOS/components/Footer.svelte
+++ src/lib/IONOS/components/Footer.svelte
@@ -8,18 +8,16 @@
 	let showTerms: boolean = false;
 </script>
 
-<div class="flex flex-col items-center">
-	<div class="flex mb-7 max-w-2xl text-gray-400 text-xs text-center gap-[3px]">
-		<p>
-			{$i18n.t('IONOS GPT can make mistakes.', { ns: 'ionos' })}
-		</p>
-		<TermsMenu
-			show={showTerms}
-			className="text-blue-700"
-		>
-			<div class="font-semibold underline">
-				{$i18n.t('Check important info.', { ns: 'ionos' })}
-			</div>
-		</TermsMenu>
-	</div>
+<div class="flex md:flex-row flex-col justify-center mx-auto mb-7 max-w-2xl text-gray-400 text-xs gap-[3px] text-center">
+	<p>
+		{$i18n.t('IONOS GPT can make mistakes.', { ns: 'ionos' })}
+	</p>
+	<TermsMenu
+		show={showTerms}
+		className="text-blue-700"
+	>
+		<div class="font-semibold underline">
+			{$i18n.t('Check important info.', { ns: 'ionos' })}
+		</div>
+	</TermsMenu>
 </div>
