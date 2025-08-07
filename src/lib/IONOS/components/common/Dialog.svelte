6cf962edcd1d025701ed344b9c6e942eaedf668f
Thomas Lehmann <t.lehmann@strato.de>
2025-08-07 19:30:51 +0200

    c/chore(Dialog): handle close by click outside
    See https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/dialog#closedby
    
    Refs: PRODAI-335

--- src/lib/IONOS/components/common/Dialog.svelte
+++ src/lib/IONOS/components/common/Dialog.svelte
@@ -28,6 +28,7 @@
 <dialog
 	on:toggle={onToggle}
 	bind:this={el}
+	closedby="any"
 	class="fixed top-0 right-0 left-0 bottom-0 m-0 bg-black/25 h-screen max-w-[100vw] w-[100vw] max-h-[100dvh] justify-center items-center z-[99999999] overflow-hidden overscroll-contain"
 	class:flex={show}
 >
