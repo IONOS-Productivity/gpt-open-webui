bf46b29c6f64aa34081e28ad4b9cb72ac52980f0
Kai Henseler <kai.henseler@strato.de>
2025-08-08 15:27:57 +0200

    c/fix(SubSettingsPage): wrong transition directions
    Refs: PRODAI-335
    
    Signed-off-by: Kai Henseler <kai.henseler@strato.de>

--- src/lib/IONOS/components/settings/SubSettingPage.svelte
+++ src/lib/IONOS/components/settings/SubSettingPage.svelte
@@ -11,7 +11,7 @@
 
 <Dialog
 	dialogId="subsetting-dialog"
-	class="p-0 md:min-h-[400px] h-screen w-screen md:max-h-fit md:min-w-[750px] md:max-w-[750px] overflow-y-scroll {show ? '-translate-x-0' : '-translate-x-full'}"
+	class="p-0 md:min-h-[400px] h-screen w-screen md:max-h-fit md:min-w-[750px] md:max-w-[750px] overflow-y-scroll {show ? 'max-md:translate-x-0' : 'max-md:translate-x-[100dvw]'}"
 	bind:show
 >
 	<div slot="header">
