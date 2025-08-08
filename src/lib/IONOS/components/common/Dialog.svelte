ba361b6f7978e18cdf5b9b3c3345f1bac683ba40
Thomas Lehmann <t.lehmann@strato.de>
2025-08-08 17:14:55 +0200

    c/feat(Dialog): prepare animating dialogs for mobile
    Refs: PRODAI-335
    
    Co-Authored-By: Kai Henseler <kai.henseler@strato.de>

--- src/lib/IONOS/components/common/Dialog.svelte
+++ src/lib/IONOS/components/common/Dialog.svelte
@@ -1,17 +1,33 @@
 <script lang="ts">
 	import { createEventDispatcher } from 'svelte';
+	import { slideDuration } from '$lib/IONOS/components/constants';
+	import { mobile } from '$lib/stores';
 
 	const dispatch = createEventDispatcher();
 
 	export let show = false;
+	/**
+	 * Cover whole viewport for mobile
+	 */
+	export let mobileCover = true;
 	export let dialogId = 'dialog';
-
 	let el: HTMLDialogElement|null = null;
 
 	$: if (show) {
 		el?.showModal();
-	} else {
+	} else if (!show && !$mobile) {
 		el?.close();
+	}
+
+	/**
+	 * Mobile: show -> false changes the transition, which then
+	 * triggers the transitionend event.
+	 * Non-mobile: immediately closed
+	 */
+	function onTransitionEnd(e) {
+		if (e.target == el && e.propertyName === 'translate' && !show) {
+			el?.close();
+		}
 	}
 
 	/**
@@ -27,9 +43,11 @@
 
 <dialog
 	on:toggle={onToggle}
+	on:transitionend={onTransitionEnd}
 	bind:this={el}
 	closedby="any"
-	class="fixed top-0 right-0 left-0 bottom-0 m-auto bg-white backdrop:bg-black/25 z-[99999999] overflow-hidden overscroll-contain shadow-xl rounded-2xl {$$props.class ?? 'p-[30px]'}"
+	style:--slide-duration="{slideDuration}ms"
+	class="fixed top-0 right-0 left-0 bottom-0 m-auto bg-white backdrop:bg-black/25 z-[99999999] overflow-hidden overscroll-contain shadow-xl rounded-2xl {mobileCover ? 'max-md:h-full max-md:max-h-dvh max-md:w-dvw max-md:max-w-dvw max-md:m-0' : ''} max-md:transition-transform duration-(--slide-duration)  ease-out {$$props.class ?? 'p-[30px]'}"
 >
 	<div
 		data-id={`dialog-${dialogId}`}
