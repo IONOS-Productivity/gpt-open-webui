035ea1a25dd3812963efd6355406123feaa078f0
Tobias Scharf <95855654+toscharfmoc@users.noreply.github.com>
2025-08-12 17:10:53 +0200

    feat: redis lock for db migration
    - Added Redis Locking to ensure only one process is updating the database at a time
    - Moved custom_resource_loader into utils package
    - customer_resource_loader is now executed after the db migration and not part of the startup_script

--- backend/dev.sh
+++ backend/dev.sh
@@ -1,15 +1,3 @@
 PORT="${PORT:-8080}"
 
-if [ -z "$RUN_MIGRATIONS" ] || [ "$RUN_MIGRATIONS" = "true" ]; then
-  # if custom_resource_loader script exists, run it
-  if [ -f "custom_resource_loader.py" ]; then
-    echo "Running maintenance script"
-    python custom_resource_loader.py
-  else
-    echo "custom_resource_loader not found"
-  fi
-else
-  echo "RUN_MIGRATIONS is either not set or set to false, skipping migrations."
-fi
-
 uvicorn open_webui.main:app --port $PORT --host 0.0.0.0 --forwarded-allow-ips '*' --reload
