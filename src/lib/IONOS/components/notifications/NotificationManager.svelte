917d24122b46db8d111a96686cb0a4d87f8b8a7f
Thomas Lehmann <t.lehmann@strato.de>
2025-08-14 09:21:31 +0200

    c/fix(NotificationManager): handle null chats
    The chats store can be null on the Explore page.
    
    Discovered via occasional exception
    
       NotificationManager.svelte?t=1755090044151:240 Uncaught (in promise) TypeError: Cannot read properties of null (reading 'length')
         at NotificationManager.svelte?t=1755090044151:240:14

--- src/lib/IONOS/components/notifications/NotificationManager.svelte
+++ src/lib/IONOS/components/notifications/NotificationManager.svelte
@@ -15,10 +15,10 @@
 	const DAYS = 24 * 60 * 60 * 1000;
 
 	// Check if the user should be prompted for feedback
-	const unsubscribeChats = chats.subscribe(async (chats: Chat[]) => {
+	const unsubscribeChats = chats.subscribe(async (chats: Chat[]|null) => {
 		const userSettings = await getUserSettings(localStorage.token);
 		const userCreatedAt = new Date($user!.created_at * 1000);
-		if (chats.length >= 2 || (new Date().getTime() - userCreatedAt.getTime()) > (14 * DAYS)) {
+		if (chats?.length >= 2 || (new Date().getTime() - userCreatedAt.getTime()) > (14 * DAYS)) {
 			if (!userSettings.ui?.ionosProvidedFeedback) {
 				addSurveyNotification();
 			}
