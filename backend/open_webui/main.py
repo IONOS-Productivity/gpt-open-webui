e9950adf7a5a1068722e4b2c52918853e95f3afe
Kai Henseler <kai.henseler@strato.de>
2025-08-19 14:21:18 +0200

    c/refactor(main.py): change pwa description for ionos
    Signed-off-by: Kai Henseler <kai.henseler@strato.de>

--- backend/open_webui/main.py
+++ backend/open_webui/main.py
@@ -1443,7 +1443,7 @@ async def get_manifest_json():
         return {
             "name": app.state.WEBUI_NAME,
             "short_name": app.state.WEBUI_NAME,
-            "description": "Open WebUI is an open, extensible, user-friendly interface for AI that adapts to your workflow.",
+            "description": "Ihr KI-Dream-Team!",
             "start_url": "/",
             "display": "standalone",
             "background_color": "#343541",
