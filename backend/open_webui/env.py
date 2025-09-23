49d921a9ea44fec364ae5083945d72f411a16d6c
Thomas Lehmann <t.lehmann@strato.de>
2025-09-23 15:35:12 +0200

    chore(backend): remove USE_CUDA related configs from code
    These configs defaulted to false.
    
    This config would have included these shared libs from Python modules
    to the LD path:
    
    * torch
    * nvidia
    
    To reduce code complexity.
    
    Refs: PRODAI-418

--- backend/open_webui/env.py
+++ backend/open_webui/env.py
@@ -31,32 +31,13 @@ try:
 except ImportError:
     print("dotenv not installed, skipping...")
 
-DOCKER = os.environ.get("DOCKER", "False").lower() == "true"
-
-# device type embedding models - "cpu" (default), "cuda" (nvidia gpu required) or "mps" (apple silicon) - choosing this right can lead to better performance
-USE_CUDA = os.environ.get("USE_CUDA_DOCKER", "false")
-
-if USE_CUDA.lower() == "true":
-    try:
-        import torch
-
-        assert torch.cuda.is_available(), "CUDA not available"
-        DEVICE_TYPE = "cuda"
-    except Exception as e:
-        cuda_error = (
-            "Error when testing CUDA but USE_CUDA_DOCKER is true. "
-            f"Resetting USE_CUDA_DOCKER to false: {e}"
-        )
-        os.environ["USE_CUDA_DOCKER"] = "false"
-        USE_CUDA = "false"
-        DEVICE_TYPE = "cpu"
-else:
-    DEVICE_TYPE = "cpu"
+DEVICE_TYPE = "cpu"
 
 try:
     import torch
 
     if torch.backends.mps.is_available() and torch.backends.mps.is_built():
+        # (apple silicon)
         DEVICE_TYPE = "mps"
 except Exception:
     pass
