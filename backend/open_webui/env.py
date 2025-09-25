ef183ed12557ae915828f2db484bd926faee495b
Thomas Lehmann <t.lehmann@strato.de>
2025-09-25 14:13:34 +0200

    chore(Dockerfile): remove WEBUI_SECRET_KEY, set config.py default to ""
    == Note on the env.py change
    
    It was defined as empty string. In Python this was overriding the
    default values as os.environ.get() treats declared variables with
    empty string defined.
    
    == Note on the default value
    
    We explicitly define this in production and dev, no need to have an
    unsecure, non-empty default value.
    
    Refs: PRODAI-418

--- backend/open_webui/env.py
+++ backend/open_webui/env.py
@@ -339,7 +339,7 @@ BYPASS_MODEL_ACCESS_CONTROL = (
 
 WEBUI_SECRET_KEY = os.environ.get(
     "WEBUI_SECRET_KEY",
-    "t0p-s3cr3t"
+    ""
 )
 
 WEBUI_SESSION_COOKIE_SAME_SITE = os.environ.get("WEBUI_SESSION_COOKIE_SAME_SITE", "lax")
