f27ef33e36be9204ea1b7caee16bc857b9acef30
Thomas Lehmann <t.lehmann@strato.de>
2025-10-01 17:06:28 +0200

    feat: remove RAG reranking via sentence_transformers.CrossEncoder
    * To reduce container image size, numbers of dependencies and unused code
    * Remove one import of dependency "sentence_transformers"
    * We now log a warning on unsupported configuration
    
    == Context
    
    * We use the embedding engine via the RAG API and don't want to run this locally
    
    == Configs removed
    
    * RAG_RERANKING_MODEL_TRUST_REMOTE_CODE
    
    Refs: PRODAI-198

--- backend/open_webui/config.py
+++ backend/open_webui/config.py
@@ -1908,10 +1908,6 @@ RAG_RERANKING_MODEL_AUTO_UPDATE = (
     and os.environ.get("RAG_RERANKING_MODEL_AUTO_UPDATE", "True").lower() == "true"
 )
 
-RAG_RERANKING_MODEL_TRUST_REMOTE_CODE = (
-    os.environ.get("RAG_RERANKING_MODEL_TRUST_REMOTE_CODE", "True").lower() == "true"
-)
-
 
 RAG_TEXT_SPLITTER = PersistentConfig(
     "RAG_TEXT_SPLITTER",
