5b753059d26a0d8cd3838a0833396d47f98acf36
Thomas Lehmann <t.lehmann@strato.de>
2025-10-14 10:33:26 +0200

    chore(+layout): remove unused code
    * There is no progress-bar
    * Remove remains of theme "her"
    
    Refs: PRODAI-7

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,5 @@
 <script>
 	import { io } from 'socket.io-client';
-	import { spring } from 'svelte/motion';
-
-	let loadingProgress = spring(0, {
-		stiffness: 0.05
-	});
 
 	import { onMount, tick, setContext } from 'svelte';
 	import {
@@ -407,35 +402,8 @@
 
 		await tick();
 
-		if (
-			document.documentElement.classList.contains('her') &&
-			document.getElementById('progress-bar')
-		) {
-			loadingProgress.subscribe((value) => {
-				const progressBar = document.getElementById('progress-bar');
-
-				if (progressBar) {
-					progressBar.style.width = `${value}%`;
-				}
-			});
-
-			await loadingProgress.set(100);
-
-			document.getElementById('splash-screen')?.remove();
-
-			const audio = new Audio(`/audio/greeting.mp3`);
-			const playAudio = () => {
-				audio.play();
-				document.removeEventListener('click', playAudio);
-			};
-
-			document.addEventListener('click', playAudio);
-
-			loaded = true;
-		} else {
-			document.getElementById('splash-screen')?.remove();
-			loaded = true;
-		}
+		document.getElementById('splash-screen')?.remove();
+		loaded = true;
 
 		return () => {
 			window.removeEventListener('resize', onResize);
