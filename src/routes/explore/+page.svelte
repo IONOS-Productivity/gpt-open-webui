e1446170fec1879c648d75c65f7f807a0cd4fa1a
pkumar <pavan.kumar@ionos.com>
2025-11-10 11:53:49 +0100

    chore: fixed typo

--- src/routes/explore/+page.svelte
+++ src/routes/explore/+page.svelte
@@ -40,7 +40,7 @@
 			return {
 				id: agentId,
 				name: $i18n.t(`name_${agentId}`, { ns: 'agents' }),
-				specialty: $i18n.t(`specialty_${agentId}`, { ns: 'agents' }),
+				specialty: $i18n.t(`speciality_${agentId}`, { ns: 'agents' }),
 				description: $i18n.t(`description_${agentId}`, { ns: 'agents' }),
 				capabilities: agentCapabilities,
 				...customAgentProps(agentId)
